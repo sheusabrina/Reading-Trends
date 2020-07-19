b6575f05f85268c7a0fa0f5f53740b0a027e5a2c
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-19 19:34:46 -0400

    created book_soup from test book

--- parser_script.py
+++ parser_script.py
@@ -2,4 +2,7 @@ from bs4 import BeautifulSoup
 
 #PARSER 1: GIVEN BOOK PAGE, GENERATES LIST OF REVIEW URLS
 
+test_book = open("test_book.html")
+book_soup = BeautifulSoup(test_book, "html.parser")
+
 #PARSER 2: GIVEN REVIEW URLS, GENERATES A DATABASE
