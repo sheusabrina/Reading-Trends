b7e952e3ac62b648695d082191b122b624da6e4c
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-19 16:30:20 -0400

    initial commit

---
+++ scraper_script.py
@@ -0,0 +1,6 @@
+#To Do List
+	#install Beautiful Soup
+	#make sure to learn about user agent strings
+	#make sure to learn about randomized sleep statements
+	#makes sure that code saves file so that you only have to run this once. 
+
