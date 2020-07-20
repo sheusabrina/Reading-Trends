957d2c5726c007cfafca2941644485db7bb270b7
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-20 16:33:34 -0400

    cleaning

--- scraper_script.py
+++ scraper_script.py
@@ -15,7 +15,7 @@ class Scraper():
     def string_to_file(self, file_name, string):
         open(file_name+".html", "w").write(string)
 
-    def sleep(self, max_sleeptime):
-        time_list = range(0, max_sleeptime, 0.1)
-        sleeptime = random.choices(time_list)
-        time.sleep(sleeptime)
+    def sleep(self, max_sleep_time):
+        time_list = range(0, max_sleep_time, 0.1)
+        sleep_time = random.choices(time_list)
+        time.sleep(sleep_time)
