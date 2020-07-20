a3caf407aff23507cf53d52408b4e910273144fe
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-19 23:40:38 -0400

    Created Parser class w/ methods html_to_soup & book_soup_to_review_urls

--- parser_script.py
+++ parser_script.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 import re
 
-#PARSER 1: GIVEN BOOK PAGE, GENERATES LIST OF REVIEW URLS
-    #Note, when this is working right, test_book will generate 5,915 URLS
+class Parser():
 
-test_book = open("test_book.html")
-book_soup = BeautifulSoup(test_book, "html.parser")
+    def __init__(self):
+        pass
 
-review_list = book_soup.find_all(attrs = {"href": re.compile("^https://www.goodreads.com/review")})
-link_list = []
-for review in review_list:
-    link = review.get("href")
-    link_list.append(link)
+    def html_to_soup(self, html):
+        soup = BeautifulSoup(html, "html.parser")
 
-#print(review_list)
+    #this method gets from the soup of a book page to urls for the top 30 reviews
+    def book_soup_to_review_urls(self, book_soup):
+        link_list = []
 
-print(link_list[0])
+        review_list = book_soup.find_all(attrs = {"href": re.compile("^https://www.goodreads.com/review")})
+        for review in review_list:
+            link = review.get("href")
+            link_list.append(link)
+
+        return link_list
+
 
 #PARSER 2: GIVEN A BOOK PAGE, CYCLES THROUGH PAGES (AJAX)
 
