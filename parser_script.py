630f45014bfaac39a3a38d064b180077c813da52
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-20 11:29:38 -0400

    add subclasses Review_Parser & Book_Parser & moved methods (untested)

--- parser_script.py
+++ parser_script.py
@@ -14,16 +14,7 @@ class Parser():
         soup = BeautifulSoup(html, "html.parser")
         return soup
 
-    def book_soup_to_review_urls(self, book_soup):
-        #this method gets from the soup of a book page to urls for the top 30 reviews
-        link_list = []
-
-        review_list = book_soup.find_all(attrs = {"href": re.compile("^https://www.goodreads.com/review")})
-        for review in review_list:
-            link = review.get("href")
-            link_list.append(link)
-
-        return link_list
+class Review_Parser(Parser):
 
     def review_soup_is_valid(self, review_soup):
         title = review_soup.find('title').get_text()
@@ -37,19 +28,22 @@ class Parser():
         review_date = review_soup.find(attrs = {"class": "right dtreviewed greyText smallText"}).get_text()
         review_date = review_date.replace("\\n" , "").strip()
 
-        print(review_date)
+        return review_date
+
+class Book_Parser(Parser):
+
+    def soup_to_review_urls(self, book_soup):
+        #this method gets from the soup of a book page to urls for the top 30 reviews
+        link_list = []
+
+        review_list = book_soup.find_all(attrs = {"href": re.compile("^https://www.goodreads.com/review")})
+        for review in review_list:
+            link = review.get("href")
+            link_list.append(link)
+
+        return link_list
 
 ##TESTING REVIEW PAGES
 
-parser_for_testing = Parser()
-
 test_review = open("test_review.html")
 test_review_error = open("test_review_error.html")
-
-test_review_soup = parser_for_testing.html_to_soup(test_review)
-#test_review_error_soup = parser_for_testing.html_to_soup(test_review_error)
-
-#print(parser_for_testing.review_soup_is_valid(test_review_soup))
-#print(parser_for_testing.review_soup_is_valid(test_review_error_soup))
-
-parser_for_testing.review_soup_to_date(test_review_soup)
