9e9401db04747e0254383cff6e66b562fcb0fab6
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-21 08:49:09 -0400

    added request header functionality to url_to_string

--- scraper_script.py
+++ scraper_script.py
@@ -16,15 +16,25 @@ class Scraper():
         self.header = random.choice(self.headers_list)
 
     def url_to_string(self, url):
-        webpage_response = requests.get(url)
+        self.select_header()
+
+        webpage_response = requests.get(url, headers = self.header)
         webpage = webpage_response.content
         webpage_string = str(webpage)
-        return webpage_string
+        self.webpage_string = webpage_string
 
-    def string_to_file(self, file_name, string):
-        open(file_name+".html", "w").write(string)
+    def string_to_file(self, file_name):
+        open(file_name+".html", "w").write(self.webpage_string)
 
     def sleep(self, max_sleep_time):
         time_list = range(0, max_sleep_time*100, 1)
         sleep_time = random.choice(time_list)/100
         time.sleep(sleep_time)
+
+##TESTING
+
+example_url = "https://docs.python.org/3/howto/regex.html"
+
+testing_scraper = Scraper()
+testing_scraper.url_to_string(example_url)
+testing_scraper.string_to_file("test_scraper_output")
