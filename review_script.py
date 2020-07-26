75cc26845f86cdeed1f741300393ea2666a93dfa
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-26 12:26:51 -0400

    delete Book, Classification, & Review classes - not going to need them

--- review_script.py
+++ review_script.py
@@ -37,24 +37,6 @@ class Review_Database():
         self.df_by_book.sort_values(by = "review_count", ascending=False, inplace = True)
         self.df_by_book.reset_index(inplace = True, drop = True)
 
-class Review():
-
-    def __init__(self, review_ID):
-        self.review_ID = review_ID
-        self.review_URL = None
-        self.is_URL_valid = None
-        self.review_publication_date = None
-        self.book_title = None
-        self.book_ID = None
-        self.rating = None
-        self.reviewer_href = None
-        self.started_reading_date = None
-        self.finished_reading_date = None
-        self.shelved_date = None
-
-        self.is_review_populated = None
-        self.review_best_date = None
-
 ## TESTING
 
 review_database = Review_Database("review_data_sample")
