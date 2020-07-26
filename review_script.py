ba355868f55fb396cb2d041ab6c4d6f9971ee313
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-26 09:15:13 -0400

    added (unpopulated) attributes to Review class

--- review_script.py
+++ review_script.py
@@ -1,6 +1,17 @@
 class Review():
 
-    def __init__(self, headline, rating, date, reviewer):
-        self.rating = rating
-        self.date = date
-        self.reviewer = reviewer
+    def __init__(self, review_ID):
+        self.review_ID = review_ID
+        self.review_URL = None
+        self.is_URL_valid = None
+        self.review_publication_date = None
+        self.book_title = None
+        self.book_ID = None
+        self.rating = None
+        self.reviewer_href = None
+        self.started_reading_date = None
+        self.finished_reading_date = None
+        self.shelved_date = None
+
+        self.is_review_populated = None
+        self.review_best_date = None
