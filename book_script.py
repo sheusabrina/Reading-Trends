97add00edb89432d9a12666278cff4d2f5bf44c1
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-07-26 09:06:03 -0400

    split up data types into seperate scripts

---
+++ book_script.py
@@ -0,0 +1,16 @@
+class Book():
+
+    def __init__(self, ISBN):
+        self.ISBN = [ISBN]
+
+    def get_ISBN_list(self):
+
+        pass
+
+    def get_metadata(self):
+
+        pass
+
+    def get_reviews(self):
+
+        pass
---
+++ review_script.py
@@ -0,0 +1,6 @@
+class Review():
+
+    def __init__(self, headline, rating, date, reviewer):
+        self.rating = rating
+        self.date = date
+        self.reviewer = reviewer
