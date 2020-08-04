626a562f02491d48ac1183962b420ba6606fc04b
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-08-04 13:02:53 -0400

    initial commit

---
+++ analysis_script.py
@@ -0,0 +1,18 @@
+#import libraries
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+
+#import classes
+
+from database_script import Book_Database
+from database_script import Review_Database
+from database_script import Merged_Database
+
+#import objects
+from database_script import merged_database
+
+class Visualizer():
+
+    pass
