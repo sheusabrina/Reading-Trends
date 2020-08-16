cdd9b6359f89f7590fcf83a0b80246d974bf4d79
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-08-16 13:44:29 -0400

    initial commit

---
+++ dist_data_collection_script.py
@@ -0,0 +1,22 @@
+#import libraries
+from bs4 import BeautifulSoup
+from datetime import datetime
+import re
+import requests
+import random
+import pandas as pd
+import sys
+
+#import classes
+
+from parser_script import Parser
+from parser_script import Review_Parser
+from parser_script import Book_Parser
+
+from scraper_script import Scraper
+
+class Boss():
+    pass
+
+class Minion():
+    pass 
