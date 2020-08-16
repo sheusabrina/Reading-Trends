386d03af857d4eceabf5bdb8521c8f9a4dc534a4
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-08-16 14:55:10 -0400

    added Review class

--- dist_data_collection_script.py
+++ dist_data_collection_script.py
@@ -17,6 +17,18 @@ from scraper_script import Scraper
 
 class Boss():
     pass
+class Review():
+
+    def __init__(id, date, book_title, book_id, rating, reviewer_href, start_date, finished_date, shelved_date):
+        self.id = review_id
+        self.date = date
+        self.book_title = book_title
+        self.book_id = book_id
+        self.rating = rating
+        self.reviewer_href = reviewer_href
+        self.start_date = start_date
+        self.finished_date = finished_date
+        self.shelved_date = shelved_date
 
 class Minion():
     pass 
