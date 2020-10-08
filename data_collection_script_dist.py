b3d07c4cb985b8b65aef9217f23e3a794955dd0f
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-08 13:13:24 -0400

    moved Book & Review classes to their own script

--- data_collection_script_dist.py
+++ data_collection_script_dist.py
@@ -17,47 +17,7 @@ import queue
 from parser_script import Review_Parser
 from parser_script import Book_Parser
 from scraper_script import Scraper
-
-#DATA STORAGE NODES
-
-class Review():
-
-    def __init__(self, id, is_valid, date, book_title, book_id, rating, reviewer_href, start_date, finished_date, shelved_date):
-        self.id = review_id
-        self.is_valid = is_valid
-        self.date = date
-        self.book_title = book_title
-        self.book_id = book_id
-        self.rating = rating
-        self.reviewer_href = reviewer_href
-        self.start_date = start_date
-        self.finished_date = finished_date
-        self.shelved_date = shelved_date
-
-    def get_data(self):
-        data = "{},{},{},{},{},{},{},{},{},{}".format(str(self.id), self.is_valid, self.current_date, self.book_title, self.book_id, self.rating, self.reviewer_href, self.start_date, self.finished_date, self.shelved_date)
-        return data
-
-class Book():
-
-    def __init__(self, book_id, author, language, num_reviews, num_ratings, avg_rating, isbn13, editions_url,publication_date,first_publication_date,series,log_time):
-        self.id = book_id
-        self.author = author
-        self.language = language
-        self.num_reviews = num_reviews
-        self.num_ratings = num_ratings
-        self.avg_rating = avg_rating
-        self.isbn13 = isbn13
-        self.editions_url = editions_url
-        self.publication_date = publication_date
-        self.first_publication_date = first_publication_date
-        self.series = series
-
-    def get_data(self):
-        data = "{},{},{},{},{},{},{},{},{},{},{}".format(self.id, self.author, self.language, self.num_reviews, self.num_ratings, self.avg_rating, self.isbn13, self.editions_url, self.publication_date, self.first_publication_date, self.series)
-        return data
-
-#DISTRIBUTED SYSTEM
+from data_classes import Book, Review
 
 class DistComponent:
 
--- data_collection_script_dist_draft.py
+++ data_collection_script_dist_draft.py
@@ -1,4 +1,4 @@
-#THIS SCRIPT WILL BE DELETED 
+#THIS SCRIPT WILL BE DELETED
 
 #THIS SCRIPT IS AN ATTEMPT TO BUILD A DISTRIBUTED SYSTEM, BUT IT NEEDS TO BE REWORKED TO USE AN HTTP SYSTEM
 #I HAVEN'T DEBUGGED IT
@@ -18,6 +18,7 @@ import sys
 from parser_script import Review_Parser
 from parser_script import Book_Parser
 from scraper_script import Scraper
+from data_classes import Book, Review
 
 ##HOW TO USE IN DISTRIBUTED SYSTEM:
     #HAVE ONE SCRIPT WITH BOSS (INIT, INPUT DATA, PREPARE)
@@ -26,51 +27,6 @@ from scraper_script import Scraper
 #REVIEW & BOOK ARE DATA STORAGE NODES:
     #GLORIFIED DICTIONARY
     #MAKE STRING STATEMENTS THAT ALIGN WITH CSV NEEDS
-
-class Review():
-
-    def __init__(self, id, is_valid, date, book_title, book_id, rating, reviewer_href, start_date, finished_date, shelved_date):
-        self.id = review_id
-        self.is_valid = is_valid
-        self.date = date
-        self.book_title = book_title
-        self.book_id = book_id
-        self.rating = rating
-        self.reviewer_href = reviewer_href
-        self.start_date = start_date
-        self.finished_date = finished_date
-        self.shelved_date = shelved_date
-
-    def get_data(self):
-        data = "{},{},{},{},{},{},{},{},{},{}".format(str(self.id), self.is_valid, self.current_date, self.book_title, self.book_id, self.rating, self.reviewer_href, self.start_date, self.finished_date, self.shelved_date)
-        return data
-
-class Book():
-
-    def __init__(self, book_id, author, language, num_reviews, num_ratings, avg_rating, isbn13, editions_url,publication_date,first_publication_date,series,log_time):
-        self.id = book_id
-        self.author = author
-        self.language = language
-        self.num_reviews = num_reviews
-        self.num_ratings = num_ratings
-        self.avg_rating = avg_rating
-        self.isbn13 = isbn13
-        self.editions_url = editions_url
-        self.publication_date = publication_date
-        self.first_publication_date = first_publication_date
-        self.series = series
-
-    def get_data(self):
-        data = "{},{},{},{},{},{},{},{},{},{},{}".format(self.id, self.author, self.language, self.num_reviews, self.num_ratings, self.avg_rating, self.isbn13, self.editions_url, self.publication_date, self.first_publication_date, self.series)
-        return data
-
-#BOSS WITH REVIEW_BOSS AND BOOK_BOSS INHERITING
-#BOSS HANDLES:
-    #SETING SCOPE OF SCRAPE & SPLITTING IT INTO ASSIGNMENTS
-    #LOGGING DATA INTO CSV
-    #PRINTING PROGRESS
-    #NEED TO BUILD FUNCTIONALITY ERRORS TO BE PASSED FROM MINION TO BOSS FOR PRINTING
-
 class Boss():
 
     def __init__(self, num_ids_per_assignment, file_name, boss_type): #INHERITED CLASSES WILL SET BOSS_TYPE
