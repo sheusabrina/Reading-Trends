e528cb0a334d82549f276ca20ff2bc4c87c23b9f
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-09 10:56:54 -0400

    get works!

--- distributed/bottle_syntax_test_requests.py
+++ distributed/bottle_syntax_test_requests.py
@@ -1,4 +1,9 @@
 import requests
 
 get_response = requests.get("http://localhost:8080/hello")
-print(get_response)
+get_content = get_response.content
+print(get_content)
+
+post_response = requests.post("http://localhost:8080/hello_dynamic", data = "Leora")
+post_content = post_response.content
+print(post_content)
