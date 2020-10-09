bf20ebdc29cfb37c6118a3406de729305445187b
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-09 11:10:47 -0400

    post errors 405...at least it runs

--- distributed/bottle_syntax_test_requests.py
+++ distributed/bottle_syntax_test_requests.py
@@ -1,8 +1,8 @@
 import requests
 
-get_response = requests.get("http://localhost:8080/hello")
-get_content = get_response.content
-print(get_content)
+#get_response = requests.get("http://localhost:8080/hello")
+#get_content = get_response.content
+#print(get_content)
 
 post_response = requests.post("http://localhost:8080/hello_dynamic", data = "Leora")
 post_content = post_response.content
