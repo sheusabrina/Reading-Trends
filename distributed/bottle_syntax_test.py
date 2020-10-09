632f58baa2824a1187d93ee707888e2bdfd7ecc9
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-08 23:15:17 -0400

    this runs

--- distributed/bottle_syntax_test.py
+++ distributed/bottle_syntax_test.py
@@ -1,11 +1,26 @@
+import bottle
 from bottle import route, run, template, post, get
 
-#BOTTLE QUICKSTART 
+#BOTTLE QUICKSTART
 
-@route('/hello')
-def hello():
-    return "Hello World!"
+#@route('/hello')
+#def hello():
+    #return "Hello World!"
+
+#run(host='localhost', port=8080, debug=True)
+
+#VALIDATION visit http://localhost:8080/hello
+
+class Rest_API:
+
+    def __init__(self):
+        pass
+
+    def generic_function(self):
+        return("Generic Function Result")
+
+my_api = Rest_API()
+
+bottle.route("/generic_url")(my_api.generic_function)
 
 run(host='localhost', port=8080, debug=True)
-
-#TEST visit http://localhost:8080/hello
