151ce1c79de386baca04dbbe1183e5c6bb2e2768
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-08 21:18:53 -0400

    added methods copied from old version

--- distributed/slave_script.py
+++ distributed/slave_script.py
@@ -41,11 +41,69 @@ class Slave_Methods():
         self.port = port
 
     def request_chunk(self):
-
         pass
 
         #self.chunk_key
         #self.chunk_items
+
+    def transmit_data(self):
+        pass
+
+    def generate_current_url(self):
+        self.current_url = self.base_url + str(self.current_id)
+
+    def scrape_url(self):
+
+        self.current_webpage_as_string = self.scraper.url_to_string_content(self.current_url)
+
+    def generate_soup(self):
+
+        self.current_soup = self.parser.html_to_soup(self.current_webpage_as_string)
+
+        if self.parser.is_soup_populated(self.current_soup): #IF THE SOUP IS POPULATED, WE'RE ALL SET
+            return
+
+        #IF THE SOUP ISN'T POPULATED, WAIT INCREASING PERIODS BETWEEN RETRIES
+
+        num_invalid_responses_recieved = 0
+
+        while self.parser.is_soup_populated(self.current_soup) == False:
+
+            if num_invalid_responses_recieved < 10:
+                #self.generate_datetime()
+                #print("Recieved Invalid Response from Website. Pausing Data Collection at {}...".format(self.self.now_string))
+
+                pause_time = max(self.max_sleep_time, num_invalid_responses_recieved*60) #IF IT'S THE FIRST ERROR, REGULAR SLEEPTIME. FOR SUBSEQUENT ERRORS, INCREASINGLY LARGE WAIT TIMES.
+                self.sleep(pause_time)
+
+                #self.generate_datetime()
+                #print("Restarting Data Collection at {}...".format(self.now_string))
+                self.current_soup = self.parser.html_to_soup(self.current_webpage_as_string)
+
+                num_invalid_responses_recieved += 1
+
+            else: #AT THE POINT WHERE IT'S TEN MINUTES BETWEEN REQUESTS, JUST TERMINATE
+
+                #print("Too Many Invalid Requests Recieved. Terminating Data Collection.")
+                sys.exit()
+
+    def log_data(self): #MINION KEEPS DATA AS A LIST OF NODES
+        self.collected_data_nodes_list.append(self.current_data_node)
+
+    def sleep(self, max_sleep_time_overwrite = None): #SYNTAX IS A LITTLE AWKWARD: IT USES THE MAX_SLEEP_TIME OF THE CLASS BY DEFAULT, BUT THAT CAN BE OVERWRITTEN
+
+        if max_sleep_time_overwrite:
+            sleeptime = max_sleep_time_overwrite
+        else:
+            sleeptime = self.max_sleep_time
+
+        self.scraper.sleep(sleeptime)
+
+    def parse(self):
+        print("This method should be overwritten in each inherited class. If this is printed, something is not working correctly.")
+
+    def generate_data_node(self):
+        print("This method should be overwritten in each inherited class. If this is printed, something is not working correctly.")
 
 class Slave(Slave_Methods):
     pass
