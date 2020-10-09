35c72365cdaed88d9962d572df9cf402ca97e2e9
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-08 21:45:37 -0400

    cleaning

--- distributed/slave_script.py
+++ distributed/slave_script.py
@@ -1,20 +1,15 @@
+#HOW TO USE SLAVE:
+    #MAKE SURE THAT MASTER SCRIPT IS ALREADY RUNNING
+    #INIT SLAVE WITH REST API HOST & PORT (MASTER'S COMPUTER)
+    #CALL DATA COLLECTION LOOP METHOD
+
 #import libraries
 from bs4 import BeautifulSoup
-from datetime import datetime
-import re
 import requests
-import random
-import pandas as pd
-import math
 import sys
 import time
 
-from bottle import route, run, template, post, get
-import threading
-import queue
-
 #import classes
-
 from parser_script import Review_Parser, Book_Parser
 from scraper_script import Scraper
 from data_classes import Book, Review
