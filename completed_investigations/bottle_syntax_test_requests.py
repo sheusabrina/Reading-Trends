5fcb3fe9c86b86ec643e53e416041c97bad8aa98
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-10 23:09:37 -0400

    cleaning

--- completed_investigations/bottle_syntax_test_requests.py
+++ completed_investigations/bottle_syntax_test_requests.py
@@ -5,23 +5,3 @@ import requests
 #post_response = requests.post("http://localhost:8080/hello_dynamic", data = {"name": "Leora"})
 #post_content = post_response.content
 #print(post_content)
-
-list_response = requests.get("http://localhost:8080/hello_list")
-list_content = list_response.content
-list_content = list_content.decode()
-list_content = list_content.split(",")
-
-chars_list = [",", "[", "]", " "]
-
-new_list = []
-
-for item in list_content:
-    for char in chars_list:
-        item = item.replace(char,"")
-    item = int(item)
-    new_list.append(item)
-
-list_content = new_list
-
-print(list_content)
-print(type(list_content))
