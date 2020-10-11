dadfde0819ff52f5b93de0a068f7155c26aca76d
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-11 00:38:23 -0400

    good enough solution to termination monitoring loop

--- distributed_data_collection/master_script.py
+++ distributed_data_collection/master_script.py
@@ -150,16 +150,24 @@ class Master_Methods():
             print("Data Collection Complete")
             sys.exit()
 
+        elif not self.chunks_oustanding_queue.empty():
+            time.sleep(10*60)
+            self.termination_monitoring_loop()
+
         elif self.chunks_oustanding_queue.empty():
 
             #SCENARIO 1: SLAVES ARE WORKING ON LAST DATA ASSIGNMENTS & WILL COME BACK IF WE WAIT
-                #SOLUTION: WAIT 
+                #SOLUTION: WAIT
             #SCENARIO 2: SLAVES ARE DOWN FOR SOME REASON & WILL NEVER COME BACK
                 #SOLUTION: ALERT HUMAN
             #SCENARIO 3: SOME SCRAPERS & ASSIGNMENTS HAVE BEEN LOST, BUT THE PROGRAM OVERALL IS RUNNING
                 #SOLUTION: REDO PREPARATION & CONTINE
+                #THIS DOESN'T NEED TO BE AUTOMATED. HUMAN CAN DO IT.
 
-            pass
+            #COMPROMISE SOLUTION: ALERT HUMAN & WAIT INDEFINITELY FOR EITHER DATA DELIVERY OR HUMAN INTERVENTION
+            print("Data collection is nearing completion or stuck. Check slave functionality") #THIS WILL BE MORE USEFUL ONCE SCRAPER CAN EMAIL ME!
+            time.sleep(3*60*60) #THREE HOURS
+            self.termination_monitoring_loop()
 
         else:
             time.sleep(10*60)
