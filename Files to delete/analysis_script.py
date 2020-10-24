d76616652dcfd431fe335542e60a24394bb6a8ce
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-10-24 14:41:56 -0400

    cleaning - renamed dfs

--- Files to delete/analysis_script.py
+++ Files to delete/analysis_script.py
@@ -5,11 +5,13 @@ import matplotlib.dates as mdates
 import seaborn as sns
 import datetime as dt
 
+#IMPORTING CURRENT REVIEW DATA
+review_df = pd.read_csv("databases/review_data.csv", low_memory = False)
+review_df.dropna(inplace = True)
+review_df.drop_duplicates(inplace = True)
 
-
-sample_df = pd.read_csv("databases/review_data.csv", low_memory = False)
-sample_df.dropna(inplace = True)
-sample_df.drop_duplicates(inplace = True)
-sample_df = sample_df[sample_df.book_title != "None"]
-sample_df = sample_df[sample_df.rating != "None"]
-review_counts = sample_df.book_title.value_counts().sort_values(ascending = False)
+#BOOK COUNTS
+review_count_by_book_df = review_df.copy()
+review_count_by_book_df = review_count_by_book_df[review_count_by_book_df.book_title != "None"]
+review_count_by_book_df = review_count_by_book_df[sample_df.rating != "None"]
+review_count_by_book_df = review_count_by_book_df.book_title.value_counts().sort_values(ascending = False)
