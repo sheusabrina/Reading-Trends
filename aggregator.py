178aaf9e1a69cbac524634361a653ef8782773f4
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-11-18 17:53:48 -0500

    added resampling (NEED HELP CHECKING)

--- aggregator.py
+++ aggregator.py
@@ -2,21 +2,24 @@ import pandas as pd
 import datetime
 import numpy as np
 
-data_file_name_book = ""
-data_file_name_review = ""
-start_date = ""
-end_date = ""
-
 class Aggregator():
 
     def __init__(self, review_file, book_file, start_date, end_date, grain):
 
+        self.start_date = start_date
+        self.end_date = end_date
+        self.grain = grain
+
+        self.check_grain()
+
         self.review_df = pd.read_csv(review_file, usecols=["review_id","is_URL_valid", "review_publication_date", "book_id"], na_values="None")
         self.book_df = pd.read_csv(book_file, usecols=["book_id" "book_language", "num_reviews", "num_ratings", "avg_rating" ,"isbn13", "book_publication_date", "book_first_publication_date", "series"], na_values="None")
 
-        self.grain = grain
-        self.start_date = start_date
-        self.end_date = end_date
+    def check_grain():
+
+        if self.grain not in ["day", "week", "month", "quarter"]:
+
+            print("Invalid Grain. Aggregator will not run correctly")
 
     def drop_invalid_rows(self, input_df):
 
@@ -52,3 +55,27 @@ class Aggregator():
 
         for df in df_list:
             df.reset_index(inplace = True, drop = True)
+
+    def resample_reviews(self): ##ASK SOMEONE TO DOUBLE CHECK THIS!
+
+        if self.grain == "day":
+            pass
+
+        elif self.grain == "week":
+            self.reviews_df["review_publication_date"] = self.reviews_df["review_publication_date"].dt.strftime('%Y-%W')
+
+        else: #GRAIN IS MONTH OR QUARTER
+
+            #FOR MONTH, USE BUILT IN
+            self.reviews_df["review_publication_date"] = self.reviews_df["review_publication_date"].dt.strftime('%Y-%m')
+
+            #FOR QUARTER, ADD ADDITIONAL PROCESSING
+
+            if self.grain = "quarter":
+                self.reviews_df["review_publication_date"] = self.reviews_df["review_publication_date"].apply(lambda month_year: "{}-{}".format(month_year.year, (month_year.month -1)//3 +1 )
+
+
+data_file_name_book = ""
+data_file_name_review = ""
+start_date = datetime.datetime(2018, 1, 1)
+end_date = datetime.datetime(2020, 2, 29)
