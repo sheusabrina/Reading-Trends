9b30a561e27863a62c4a408c8e344749d03e2d90
leora-dot <7536446+leora-dot@users.noreply.github.com>
2020-11-19 16:20:12 -0500

    some weird nuance around how the boolean text is stored

--- aggregator.py
+++ aggregator.py
@@ -36,7 +36,8 @@ class Aggregator():
 
     def drop_invalid_reviews(self):
 
-        self.review_df = self.review_df[self.review_df.is_URL_valid == "True"]
+        self.review_df = self.review_df[self.review_df.is_URL_valid != "FALSE"]
+        self.review_df = self.review_df[self.review_df.is_URL_valid != "False"]
         self.review_df.drop(columns = "is_URL_valid", inplace = True)
 
     def datetime_conversion(self, input_df):
